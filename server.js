5a91c3d7b2a0e7bf638239d407490b46f64573a8
Nodnarb3 <brandon.slack@live.ca>
2019-02-24 17:23:45 -0700

    * Basic layout WIP

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const url = require("url");
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/requestUserToken", (req, res) =>{
+	res.send("Nope");
+});
