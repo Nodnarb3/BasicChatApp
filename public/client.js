a3edebbe1d0025eef0e22e21cd57bda3631102d6
Nodnarb3 <brandon.slack@live.ca>
2019-02-26 21:49:29 -0700

    + Added Joining / Leaving Socket.io stuffs

--- public/client.js
+++ public/client.js
@@ -1,5 +1,44 @@
 (function(){
+	let socket = io();
+	socket.on("connect", connected);
+	socket.on("token", (data) => {setToken(data);});
+	socket.on("join", (data) => {userJoined(data);});
+	socket.on("leave", (data) => {userLeft(data);});
 	
-	console.log("HelloWorld");
+	
+	function connected()
+	{
+		if(!!Cookies.get("token"))
+		{
+			//Reset expiry date
+			let currentToken = Cookies.get("token");
+			setToken(currentToken);
+		}
+		
+	}
+	
+	function setToken(data)
+	{
+		console.log("Token: " + data);
+		Cookies.set("token", data, {expires: 1});
+		
+		$(".your-nick").html(JSON.parse(Cookies.get("token")).nick);
+		
+	}
+	
+	function userJoined(data)
+	{
+		let newUser = JSON.parse(data);
+		//Show msg in chat
+		//Add to users list
+		$("#user-list").append("<div data-uuid='"+ newUser.uuid +"' class='message'>"+ newUser.nick +"</div>");
+	}
+	
+	function userLeft(data)
+	{
+		let leftUser = JSON.parse(data);
+		console.log(leftUser.uuid);
+		$("#user-list").find("[data-uuid='"+ leftUser.uuid +"']").remove();
+	}
 	
 })();
--- server.js
+++ server.js
@@ -1,12 +1,67 @@
 const express = require("express");
 const url = require("url");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const uuid = require("uuid/v4");
+const cookie = require("cookie");
 const port = 8080;
+
+let userCounter = 0;
+
+
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get("/requestUserToken", (req, res) =>{
-	res.send("Nope");
+io.on('connection', function(socket){	
+	let user;
+	
+	if(!!socket.handshake.headers.cookie)
+	{
+		let c = cookie.parse(socket.handshake.headers.cookie);
+		if(!!c.token)
+		{
+			user = c.token.toString();
+			console.log("Reconnected User - " + user);
+		}
+		else
+		{
+			user = JSON.stringify(createUser(uuid(), genNickname()));
+			socket.emit('token', user);
+			console.log("New User - " + user);
+		}
+	}
+	else
+	{
+		user = JSON.stringify(createUser(uuid(), genNickname()));
+		socket.emit('token', user);
+		console.log("New User - " + user);
+
+	}
+	
+	io.emit("join", user);
+	
+	socket.on('disconnecting', function(){
+		io.emit("leave", user);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log("Disconnected: " + user);
+  });
 });
+
+function genNickname()
+{
+	userCounter++;
+	return "Anon-" + userCounter; 
+}
+
+function createUser(uuid, nick)
+{
+	return {
+		uuid,
+		nick
+	};
+}
