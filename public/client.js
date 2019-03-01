a6d4c3d7f84ffa4cad2a5b06a49470be300780bf
Nodnarb3 <brandon.slack@live.ca>
2019-02-28 17:13:04 -0700

    Lots of changes!

--- public/client.js
+++ public/client.js
@@ -4,7 +4,29 @@
 	socket.on("token", (data) => {setToken(data);});
 	socket.on("join", (data) => {userJoined(data);});
 	socket.on("leave", (data) => {userLeft(data);});
+	socket.on("user-list", (data) => {updateUserList(data);});
+	socket.on("message", (data) => {appendMessage(data);});
+	socket.on("message-history", (data) => {chatHistory(data);});
+	socket.on("update-nick", (data) => {setToken(data);});
 	
+	
+	$('#msg-input').on("keypress", (event)=>{
+		if(event.which == 13) 
+		{
+        	sendMsg();
+    	}
+	});
+	
+	$("#send-btn").on('click', (event)=>{
+		sendMsg();
+	});
+	
+	function sendMsg()
+	{
+		let msg = $('#msg-input').val();
+		socket.emit("message", msg);
+		$('#msg-input').val(null);
+	}
 	
 	function connected()
 	{
@@ -29,16 +51,87 @@
 	function userJoined(data)
 	{
 		let newUser = JSON.parse(data);
-		//Show msg in chat
 		//Add to users list
-		$("#user-list").append("<div data-uuid='"+ newUser.uuid +"' class='message'>"+ newUser.nick +"</div>");
+		let html = ejs.render("<div data-uuid='<%=user.uuid%>' class='user' style='color:<%=user.color%>;'><%=user.nick%></div>", {user: newUser});
+		
+		$("#user-list").append(html);
 	}
 	
 	function userLeft(data)
 	{
 		let leftUser = JSON.parse(data);
-		console.log(leftUser.uuid);
 		$("#user-list").find("[data-uuid='"+ leftUser.uuid +"']").remove();
+	}
+	
+	function addChatMessage(auth, msg, timestamp)
+	{	
+		let html = ejs.render(		
+		`<div data-uuid="<%=auth.uuid%>" class="message-wrapper">
+			<div class="message-inner" style="background-color: <%=auth.color%>4F">
+				<div class="message-user"><%=auth.nick%>:</div>
+				<div class="message-body"><%-msg%></div>
+			</div>
+		</div>`,{auth: auth, msg: msg});
+		
+		if(isMe(auth.uuid))
+		{
+			html = $(html).addClass("mine");
+		}
+		
+		$("#message-list").append(html);
+		$("#message-list").animate({scrollTop: $("#message-list")[0].scrollHeight}, 1);
+		
+		
+	}
+	
+	function updateUserList(data)
+	{
+		let users = JSON.parse(data);
+		
+		$("#user-list").html(null);
+		
+		users.forEach(function(user){
+			let html = ejs.render("<div data-uuid='<%=user.uuid%>' class='user' style='color:<%=user.color%>;'><%=user.nick%></div>", {user: JSON.parse(user)});
+		
+			$("#user-list").append(html);
+					
+		});
+		
+		makeMeBold();
+	}
+	
+	function appendMessage(data)
+	{
+		let msgData = JSON.parse(data);
+		
+		addChatMessage(JSON.parse(msgData.user), msgData.msg, msgData.timestamp);
+	}
+	
+	function chatHistory(data)
+	{
+		let chat = JSON.parse(data);
+		
+		$("#message-list").html(null);
+		
+		chat.forEach((msg)=>{			
+			addChatMessage(JSON.parse(msg.user), msg.msg, msg.timestamp);
+		});
+				
+	}
+	
+	function isMe(id)
+	{
+		let myUuid = JSON.parse(Cookies.get("token")).uuid;
+
+		return id === myUuid;
+		
+	}
+		
+	function makeMeBold()
+	{
+		let myUuid = JSON.parse(Cookies.get("token")).uuid;
+		
+		$('[data-uuid="'+ myUuid +'"]').addClass('bold');
 	}
 	
 })();
--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const uuid = require("uuid/v4");
 const cookie = require("cookie");
 const port = 8080;
 
+const serverUser = JSON.stringify(createUser("server", "!", "#ffffff"));
+const serverWarning = JSON.stringify(createUser("server", "!", "#ff0000"));
+
 let userCounter = 0;
 
-
+let users = [];
+let messages = [];
 
 app.use(express.static('public'));
 
@@ -28,29 +32,81 @@ io.on('connection', function(socket){
 		}
 		else
 		{
-			user = JSON.stringify(createUser(uuid(), genNickname()));
+			user = JSON.stringify(createUser(uuid(), genNickname(), genColor()));
 			socket.emit('token', user);
 			console.log("New User - " + user);
 		}
 	}
 	else
 	{
-		user = JSON.stringify(createUser(uuid(), genNickname()));
+		user = JSON.stringify(createUser(uuid(), genNickname(), genColor()));
 		socket.emit('token', user);
 		console.log("New User - " + user);
-
 	}
 	
+	users.push(user);
+	
+	socket.emit("message-history", JSON.stringify(messages));
 	io.emit("join", user);
+	socket.emit("user-list", JSON.stringify(users));
+	io.emit("message", JSON.stringify(createMessage(serverUser, JSON.parse(user).nick + " has joined the server!")));
+	
 	
 	socket.on('disconnecting', function(){
 		io.emit("leave", user);
+		io.emit("message", JSON.stringify(createMessage(serverUser, JSON.parse(user).nick + " has left the server!")));
 	});
 	
 	socket.on('disconnect', function(){
+		users.splice(users.indexOf(user), 1);
 		console.log("Disconnected: " + user);
-  });
+		console.log(users);
+  	});
+	
+	socket.on('message', function(msg){
+		
+		if(msg.startsWith("/nick "))
+		{
+			console.log("Updating username for user: " + user);
+			user = updateNickname(socket, msg, user);
+			console.log(user);
+		}
+		else
+		{
+			let m = createMessage(user, msg);
+			messages.push(m)
+			io.emit('message', JSON.stringify(m));	
+		}
+		
+	});
 });
+
+function updateNickname(socket, msg, user)
+{
+	let commandTokens = msg.split(" ");
+			
+	if(commandTokens.length !== 2 || commandTokens[1].length <= 0)
+	{
+		socket.emit("message", JSON.stringify(createMessage(serverWarning, 'Invalid number of arguments.')));
+		socket.emit("message", JSON.stringify(createMessage(serverWarning, 'The format expected is:')));
+		socket.emit("message", JSON.stringify(createMessage(serverWarning, '/nick &lt;new_nickname&gt;')));
+		
+		return user;
+	}
+	
+	let u = JSON.parse(user);
+	let newNick = commandTokens[1];
+	
+	u.nick = newNick;
+	
+	let newUser = JSON.stringify(u);
+	
+	users[users.indexOf(user)] = newUser;
+		
+	socket.emit("update-nick", newUser);
+	
+	return newUser;
+}
 
 function genNickname()
 {
@@ -58,10 +114,34 @@ function genNickname()
 	return "Anon-" + userCounter; 
 }
 
-function createUser(uuid, nick)
+function createUser(uuid, nick, color)
 {
 	return {
 		uuid,
-		nick
+		nick,
+		color
 	};
+}
+	
+function createMessage(user, message)
+{
+	return {
+		user,
+		msg: message,
+		timestamp: new Date().getTime()
+	};
+}
+
+function genColor()
+{
+  	let characters = "0123456789ABCDEF";
+	let color = "#";
+	
+	for(let i = 0; i < 6; i++)
+	{
+		color += characters[Math.floor(Math.random() * characters.length)]; 
+	}
+	
+	return color;
+
 }
